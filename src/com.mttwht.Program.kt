5fbddca7609fe5027334b340287cfe3cf0bb1e07
Matthew White <Matthew.White02@gmail.com>
2019-09-11 09:35:47 +0100

    Declare functions

---
+++ src/com.mttwht.Program.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    display("Hello, World")
+    println(max(1,2))
+}
+
+fun display(message: String) : Boolean {
+    println(message)
+    return true
+}
+
+fun max(a: Int, b: Int) : Int = if(a >= b) a else b
